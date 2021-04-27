51753816be4613e61ac9245a27cbd906d7a667e3
Igor <igoraraujocruzz@gmail.com>
2021-04-27 11:31:02 -0300

    Add Class Error
    If applied, this commit will create a global class
    of errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import dontenv from 'dotenv';
 import '@shared/infra/typeorm';
-import express from 'express';
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 import '@shared/container';
 
@@ -9,6 +11,24 @@ const app = express();
 dontenv.config();
 app.use(express.json());
 app.use(routes);
+
+app.use(
+    (error: Error, _request: Request, response: Response) => {
+        if (error instanceof AppError) {
+            const { statusCode } = error;
+
+            return response.status(statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
 
 app.listen(3335, () => {
     console.log('Server started');
